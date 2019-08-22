0fdf97f01b4ed5befe86c6c6ee775da702899a18
Jo Jinsik <kunggom@gmail.com>
2019-08-22 16:10:25 +0900

    refactor(comment): Controller에서 UserService의 기능을 사용하도록 변경

--- src/main/java/com/woowacourse/zzinbros/comment/controller/CommentController.java
+++ src/main/java/com/woowacourse/zzinbros/comment/controller/CommentController.java
@@ -2,15 +2,13 @@ package com.woowacourse.zzinbros.comment.controller;
 
 import com.woowacourse.zzinbros.comment.domain.Comment;
 import com.woowacourse.zzinbros.comment.dto.CommentRequestDto;
-import com.woowacourse.zzinbros.comment.exception.UnauthorizedException;
 import com.woowacourse.zzinbros.comment.service.CommentService;
 import com.woowacourse.zzinbros.post.domain.Post;
 import com.woowacourse.zzinbros.post.service.PostService;
 import com.woowacourse.zzinbros.user.domain.User;
+import com.woowacourse.zzinbros.user.dto.UserResponseDto;
 import com.woowacourse.zzinbros.user.service.UserService;
 import org.springframework.web.bind.annotation.*;
-
-import javax.servlet.http.HttpSession;
 
 @RestController
 @RequestMapping("/comments")
@@ -25,32 +23,23 @@ public class CommentController {
         this.userService = userService;
     }
 
-    // TODO: 차후 UserService에서 현재 로그인된 유저를 가져오는 기능을 제공하면 삭제
-    private User getLoggedInUser(final HttpSession session) {
-        final User user = (User) session.getAttribute("loggedInUser");
-        if (user == null) {
-            throw new UnauthorizedException();
-        }
-        return user;
-    }
-
     @PostMapping
-    public Comment add(@RequestBody final CommentRequestDto dto, final HttpSession session) {
-        final User user = getLoggedInUser(session);
+    public Comment add(@RequestBody final CommentRequestDto dto, final UserResponseDto loginUserDto) {
+        final User user = userService.findLoggedInUser(loginUserDto);
         final Post post = postService.read(dto.getPostId());
         final String contents = dto.getContents();
         return commentService.add(user, post, contents);
     }
 
     @PutMapping
-    public Comment edit(@RequestBody final CommentRequestDto dto, final HttpSession session) {
-        final User user = getLoggedInUser(session);
+    public Comment edit(@RequestBody final CommentRequestDto dto, final UserResponseDto loginUserDto) {
+        final User user = userService.findLoggedInUser(loginUserDto);
         return commentService.update(dto.getCommentId(), dto.getContents(), user);
     }
 
     @DeleteMapping
-    public boolean delete(@RequestBody final CommentRequestDto dto, final HttpSession session) {
-        final User user = getLoggedInUser(session);
+    public boolean delete(@RequestBody final CommentRequestDto dto, final UserResponseDto loginUserDto) {
+        final User user = userService.findLoggedInUser(loginUserDto);
         try {
             commentService.delete(dto.getCommentId(), user);
             return true;
